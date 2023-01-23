e5cf8ec994887c0ae604de00cb19d30417dbc375
Rodolfo Faria <106396728+rodolforsf@users.noreply.github.com>
2023-01-23 16:42:06 -0300

    Parte lógica implementada

--- src/app/components/main-card/main-card.component.ts
+++ src/app/components/main-card/main-card.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-main-card',
   templateUrl: './main-card.component.html',
-  styleUrls: ['./main-card.component.css','./main-card.responsive.component.css']
+  styleUrls: ['./main-card.component.css', './main-card.responsive.component.css']
 })
 export class MainCardComponent {
 
+  @Input()
+  photoCover:string = ""
+  @Input()
+  cardTitle:string = ""
+  @Input()
+  cardDescription:string = ""
+
+  constructor() {
+
+
+  }
+
+  ngOnInit(): void {
+
+  }
 }
--- src/app/components/other-card/other-card.component.ts
+++ src/app/components/other-card/other-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-other-card',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class OtherCardComponent {
 
+  @Input()
+  photoCover:string = ""
+
+  @Input()
+  cardTitle:string = ""
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 }
